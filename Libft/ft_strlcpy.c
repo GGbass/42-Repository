351584b88a5e13fa5510bea9ff4f8a8ddc7e34bb
Gonzalo Garcia Bastidas <ggonzalo.bastidas@gmail.com>
2023-09-21 23:52:03 +0200

    ft_strlcpy.c without main

---
+++ Libft/ft_strlcpy.c
@@ -0,0 +1,26 @@
+#include <libft.h>
+//#include <stdio.h>
+//#include <string.h>
+
+size_t	ft_strlcpy(char *dest, const char *src, size_t size)
+{
+	size_t	i;
+
+	i = 0;
+	if (size == 0)
+	{
+		while(src[i])
+			i++;
+		return (i);
+	}
+	while (i < size -1 && i != '\0')
+	{
+		dest[i] == src[i];
+		i++;
+	}
+	if (i < size)
+		dest[i] = '\0';
+	while(src[i] != '\0');
+			i++;
+	return (i);
+}

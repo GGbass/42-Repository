b1fe1bd67a80d667556068bcf1219713f99b8238
GGbass <102612939+GGbass@users.noreply.github.com>
2023-09-25 15:20:35 +0200

    Main comentado
    ejemplo comentado sobre el desbordamiento de buffer

--- libft/ft_memcpy.c
+++ libft/ft_memcpy.c
@@ -1,2 +1,35 @@
 #include <libft.h>
+//#include <string.h>
+//#include <stdio.h>
 
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	size_t	i;
+	char	*p_dest;
+	char	*p_src;
+
+	i = 0;
+	p_dest = (char *)dest;
+	p_src = (char *)src;
+	while (i < n)
+	{
+		p_dest[i] = p_src[i];
+		i++;
+	}
+	return (dest);
+	
+}
+/* if (dest != NULL && src != NULL && n > 0) {
+  ft_memcpy(dest, src, n);
+} else {
+  // Desbordamiento de buffer script.
+}*/
+/*int	main()
+{
+	char	dest[7];
+	char	src[] = "Hola Mundo!";
+
+	ft_memcpy(dest, src, sizeof(src));
+	printf("Resultado de dest es %s\n :", dest);
+	return (0);
+}*/

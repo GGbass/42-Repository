3adc6827b5b3e4a8c2f960a39e6ca84299c22d7b
GGbass <102612939+GGbass@users.noreply.github.com>
2023-09-28 14:33:19 +0200

    Update

--- libft/libft.h
+++ libft/libft.h
@@ -25,26 +25,26 @@ int		ft_toupper(int i);
 int		ft_tolower(int i);
 int		ft_isprint(int i);
 /*<string.h>*/
-void	*ft_memset(void *s, int c, size_t n);
-int		ft_strlen(const char *str);
-void	ft_bzero(void *s, size_t n);
-size_t	ft_strlcpy(char *dest, const char *src, size_t size);
-char	*ft_strchr(const char *s, int i);
 char	*ft_strrchr(const char *s, int c);
-int		ft_strncmp(const char *s1, const char *s2, size_t n);
-char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
-void	*ft_memcpy(void *dest, const void *src, size_t n);
+char	*ft_strchr(const char *s, int c);
+int		ft_strlen(const char *str);
+void	*ft_memset(void *s, int c, size_t n);
+void	ft_bzero(void *s, size_t n);
 size_t	ft_strlcat(char *dest, const char *src, size_t size);
+size_t	ft_strlcpy(char *dest, const char *src, size_t size);
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t bytes_copied);
+int		ft_memcmp(const void *dest, const void *src, size_t n_bytes);
+void	*ft_memchr(const void *s, int c, size_t n_bytes);
+int		ft_atoi(const char *str);
 /*<stdlib.h>*/
 
 /* 42 functions*/
 #endif
 /**
  *functions missing:
- memchr
- memcmp
- atoi
  usingmalloc :
  calloc
  strdup

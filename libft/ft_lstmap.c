a0ab3211d4031795df685aaf3098b179f9c8e6be
GGbass <102612939+GGbass@users.noreply.github.com>
2023-10-18 15:46:46 +0200

    Add files via upload

--- libft/ft_lstmap.c
+++ libft/ft_lstmap.c
@@ -15,13 +15,27 @@
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*new_list;
-	t_list	*save;
+	t_list	*new_element;
 
 	if (!lst || !f || !del)
 		return (NULL);
-	return (0);
+	new_list = ft_lstnew(f(lst->content));
+	if (!new_list)
+		return (NULL);
+	lst = lst->next;
+	while (lst)
+	{
+		new_element = ft_lstnew(f(lst->content));
+		if (!new_element)
+		{
+			ft_lstclear(&new_list, del);
+			return (NULL);
+		}
+		ft_lstadd_back(&new_list, new_element);
+		lst = lst->next;
+	}
+	return (new_list);
 }
-
 
 /*int	main()
 {
